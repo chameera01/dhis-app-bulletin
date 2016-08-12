be1400e5982e8c18ed5fb99f342c33bfcbd54010
chameera01 <chameera9019@gmail.com>
2016-08-12 18:21:30 +0530

    all pages downloadable, pages are A4 size

--- js/dhis-bulletin.js
+++ js/dhis-bulletin.js
@@ -1,18 +1,359 @@
 var bulletin = bulletin || {};
-var baseURL = "http://localhost:8080/dhisv220/";
+var baseURL = "http://hospdev.hispindia.org/uphmis_testing/";
 
-// Login - if OK, call the setLinks function
+/* contain all DBs and DB item details */
+bulletin.DBs = {};
 
-/*Ext.onReady( function() {
-  Ext.Ajax.request({
-	url: base + "/dhis-web-commons/security/login.action",
-	method: "POST",
-	params: { j_username: "akash", j_password: "Akash@123" },
-	success: setLinks
-  });
-});*/
+/* contain all page IDs in an order*/
+bulletin.pageIDs = [];
+
+/* contain index of current page in bulletin.pageIDs */
+bulletin.currentPage = -1;
+
+/*  an array of objects {tableID,elementID}
+ *  contain all pivot table's ID and element ID
+ *  have to use this approach bcz pivot tables are generated using dhis2 plugin */
+bulletin.pivotTables = [];
 
 
+var callAPI = function( url, filter ){
+	var def = $.Deferred();
+
+	$.ajax({
+		url: url,
+		type: 'GET',
+		data: filter
+	}).done( function( response ){
+		console.log("api call done");
+		def.resolve(response);
+	}).fail(function(){
+		console.log("api call fail");
+		def.resolve();
+	});
+
+	return def.promise();
+};
+
+/* retrieve all dashboards info */
+var loadDBsInfo = function(){
+
+	var def = $.Deferred();
+
+	callAPI('../../../api/dashboards.json?fields=:all,dashboardItems[:all]','dummy filter').then(function(response){
+
+		bulletin.DBs = response.dashboards;
+
+		def.resolve();
+	});
+
+
+	return def.promise();
+};
+
+/*
+ * generate html content related to all Dashbord pages and
+ * write to a div
+ * @param DBs - bulletin.DBs
+ */
+var generateDBPages = function(DBs){
+
+	var DBPagesContent = '';
+
+	/* navigation UI component */
+	DBPagesContent += '<nav aria-label="...">'+
+		'<ul class="pager">'+
+		'<li id="preBtn" class="previous" onclick="return navigatePages(-1);"><a href="#"><span aria-hidden="true">&larr;</span> previous</a></li>'+
+		'<li id="nextBtn" class="next" onclick="return navigatePages(+1);"><a href="#">Next <span aria-hidden="true">&rarr;</span></a></li>'+
+		'</ul>'+
+		'</nav>'
+		'';
+
+	$.each(DBs, function (index, DB) {
+		DBPagesContent += generateDBPage(index,DB);
+	});
+
+	document.getElementById("dhisBulletin").innerHTML = DBPagesContent;
+
+	generatePivotTables();
+
+	navigatePages(+1);
+};
+
+/*
+ * @param pageNo - start from 0,1,2,...
+ * @param DB - one DashBord per one page - bulletin.DBs[i]
+ * @returns {string} - html content related to that page
+ */
+var generateDBPage = function(pageNo,DB){
+
+	var DBPageContent = '';
+	DBPageContent += '<div id="dbPage-'+pageNo+'" class="page">';
+
+	DBPageContent += '<div class="page-title"><h4>'+DB.displayName+'</h4></div>';
+
+	$.each(DB.dashboardItems, function (index, DBItem) {
+		DBPageContent += generateDBItem(pageNo,index,DBItem);
+	});
+
+	DBPageContent += '<div class="inline moveable">'+
+						'<textarea rows="4" cols="50">'+
+							'this is a text area template...'+
+						'</textarea>'+
+					'</div>';
+
+	DBPageContent += '<div id="page-footer"></div>';
+
+	DBPageContent += '</div>';
+
+	bulletin.pageIDs.push("dbPage-"+pageNo+"");
+
+	return DBPageContent;
+};
+
+/*
+ * generate html content depend on item type(pivotTable,chart,...)
+ * @param pageNo - start from 0,1,2,...
+ * @param itemNo - start from 0,1,2,...
+ * @param DBItem - bulletin.DBs[i].dashboardItems[j]
+ * @returns {string} - html content of that item
+ */
+var generateDBItem = function(pageNo,itemNo,DBItem){
+
+	var DBItemContent = '';
+	var itemType = getDBItemType(DBItem);
+
+	switch(itemType) {
+		case "reportTable":
+			var itemID = DBItem.reportTable.id;
+			DBItemContent = '<div id="dbItem-'+pageNo+'-'+itemNo+'" class="dbItemDiv inline">' +
+				'</div>';
+
+			bulletin.pivotTables.push({tableID:""+itemID+"",elementID:"dbItem-"+pageNo+"-"+itemNo+""});
+			break;
+		case "chart":
+			var itemID = DBItem.chart.id;
+			DBItemContent = '<div id="dbItem-'+pageNo+'-'+itemNo+'" class="dbItemDiv inline">' +
+				'<img src="'+ baseURL +'api/charts/'+itemID+'/data?width=410&height=275" alt="db item">' +
+				'</div>';
+			break;
+		default:
+			DBItemContent = "invalid item";
+	}
+
+	return DBItemContent;
+};
+
+/*
+ * call dhis2 plugin
+ * bulletin.pivotTables contain all details
+ */
+var generatePivotTables = function() {
+
+	$.each(bulletin.pivotTables, function (index, table) {
+		DHIS.getTable({ url: baseURL, el: table.elementID, id: table.tableID });
+	});
+
+	//DHIS.getTable({ url: baseURL, el: "dbItem-0-0", id: "j6YjtD8oj5S" });
+
+	//setTimeout(resizeTable, 4000);
+	//$("#dbItem-0-0").children("table").addClass("test");
+	//resizeTable($("#dbItem-0-0").children("table"),100,100);
+	//resizeTable($("#dbItem-0-0").children());
+	//$("#dbItem-0-0_bec73fca-11de-41ec-be9d-a56fcecc26a1").addClass("test");
+};
+
+/*
+ * @param item - bulletin.DBs[i].dashboardItems[j]
+ * @returns {string}
+ */
+var getDBItemType = function(item){
+	var type = "invalid";
+
+	switch(item.type) {
+		case "reportTable":
+			type = "reportTable";
+			break;
+		case "chart":
+			type = "chart";
+			break;
+		default:
+		type = "invalid";
+	}
+
+	return type;
+};
+
+/*
+ * not used
+ * @param DBItem
+ * @returns {*|string}
+ */
+var getDBItemID = function(DBItem){
+	return DBItem.id;
+};
+
+/*
+ * will hide all other page divs and display only one
+ * @param pageID - bulletin.pageIDs[i]
+ */
+var displayPage = function(pageID){
+	/*$.each(bulletin.pageIDs, function (index, pageID) {
+		if(pageID == visibleID)
+			$("#"+pageID+"").attr("style","display:initial");
+		else
+			$("#"+pageID+"").attr("style","display:none");
+	});*/
+
+	$(".page").addClass("hidden");
+	$("#"+pageID+"").removeClass("hidden");
+};
+
+/*
+ * navigate through pages
+ * @param direction - -1 for previous page and +1 for next page
+ * @returns {boolean}
+ */
+var navigatePages = function(direction){
+
+	var newPage = bulletin.currentPage + direction;
+
+	/* this condition can be removed for now */
+	if( newPage < 0 || bulletin.pageIDs.length <= newPage){
+		alert("invalid navigation");
+		return false;
+	}
+
+	displayPage(bulletin.pageIDs[newPage]);
+	window.location.hash="page-"+(newPage+1);
+	bulletin.currentPage = newPage;
+
+	/* following conditions are related to navigation UI component used */
+	if(bulletin.pageIDs.length <= newPage+1)
+		$("#nextBtn").addClass("disabled");
+	else
+		$("#nextBtn").removeClass("disabled");
+
+	if(newPage-1 < 0)
+		$("#preBtn").addClass("disabled");
+	else
+		$("#preBtn").removeClass("disabled");
+
+	return false;
+};
+
+/*
+ * download a single page as an image using html2canvas library
+ * @param pageID - bulletin.pageIDs[i]
+ * @param pageName - downloaded file gets this name
+ */
+var downloadPage = function(pageID,pageName){
+
+/*	var myBlob = new Blob( [document.getElementById('dhisBulletin').outerHTML] , {type: 'image/png'});
+	var url = window.URL.createObjectURL(myBlob);
+	var a = document.createElement("a");
+	document.body.appendChild(a);
+	a.href = url;
+	a.download = "newfile.png";
+	a.click();
+	//adding some delay in removing the dynamically created link solved the problem in FireFox
+	setTimeout(function() {window.URL.revokeObjectURL(url);},0);*/
+
+	//saveSvgAsPng(document.getElementById("dhisBulletin"), "dhisBulletin.png");
+
+	/*html2canvas(document.body).then(function(canvas) {
+		//document.body.appendChild(canvas);
+
+		console.log(canvas);
+
+		link.href = canvas.toDataURL();
+		link.download = newfile.png;
+
+		/!*var url = window.URL.createObjectURL(canvas);
+		var a = document.createElement("a");
+		document.body.appendChild(a);
+		a.href = url;
+		a.download = "newfile.png";
+		a.click();*!/
+
+		//adding some delay in removing the dynamically created link solved the problem in FireFox
+		//setTimeout(function() {window.URL.revokeObjectURL(url);},0);
+	});*/
+
+	displayPage(pageID);
+
+	html2canvas($("#"+pageID+""),
+		{
+			onrendered: function (canvas) {
+				var a = document.createElement("a");
+				// toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+				//a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+				a.href = canvas.toDataURL("image/png");
+				a.download = pageName + ".png";
+				a.click();
+			}
+		});
+
+	displayPage(bulletin.pageIDs[bulletin.currentPage]);
+
+};
+
+/*
+ * download all pages
+ */
+var downloadBulletin = function(){
+
+	$.each(bulletin.pageIDs, function (index, pageID) {
+		var pageName = "bulletin-page-"+(index+1);
+		downloadPage(pageID,pageName);
+	});
+
+};
+
+var printBulletin = function(){
+
+	$(".page").removeClass("hidden");
+	window.print();
+
+	displayPage(bulletin.pageIDs[bulletin.currentPage]);
+};
+
+/*
+ * dont use for now
+ * @param tableID
+ * @param width
+ * @param height
+ */
+var resizeTable = function(tableID,width,height){
+
+	console.log($("#dbItem-0-0").children());
+	$("#dbItem-0-0").children("table").attr("width","100");
+	//console.log($("#dbItem-0-0"));
+	//$("#"+tableID+"").attr({"width":""+width+"", height:""+height+""});
+	//tableID.attr({"width":""+width+"", "height":""+height+""});
+	//tableID.attr("id","helloo");
+	//tableID.prevObject[0].clientHeight = 400;
+};
+
+$(document).ready(function(){
+
+	var def = $.Deferred();
+	var promise = def.promise();
+
+	promise = promise.then(loadDBsInfo);
+	promise = promise.then(function(){
+		console.log(bulletin.DBs);
+		generateDBPages(bulletin.DBs);
+	});
+
+	def.resolve();
+
+});
+
+
+
+
+
+/* following code is used for drag and drop feature */
 function drag_start(event) {
 	var style = window.getComputedStyle(event.target, null);
 	event.dataTransfer.setData("application/x-moz-node", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY) + ',' + event.target.getAttribute('data-item'));
@@ -36,183 +377,10 @@ function drop(event) {
 }
 
 /*
-var mv = document.getElementsByClassName('moveable');
-console.log("hello..*.");
-for (var i = 0; i < mv.length; i++) {
-	mv[i].addEventListener('dragstart', drag_start, false);
-	document.body.addEventListener('dragover', drag_over, false);
-	document.body.addEventListener('drop', drop, false);
-}*/
-
-
-var apiCall = function( url, filter ){
-	var def = $.Deferred();
-
-	$.ajax({
-		url: url,
-		type: 'GET',
-		data: filter
-	}).done( function( response ){
-		console.log("api call done");
-		def.resolve(response);
-	}).fail(function(){
-		console.log("api call fail");
-		def.resolve();
-	});
-
-	return def.promise();
-};
-
-var loadDBsInfo = function(){
-
-	var def = $.Deferred();
-
-	apiCall('../api/dashboards.json?fields=:all,dashboardItems[:all]','dummy filter').then(function(response){
-
-		bulletin.DBs = response.dashboards;
-
-		def.resolve();
-	});
-
-
-	return def.promise();
-};
-
-var generateDBPages = function(DBs){
-
-	/* declare an array to store all page IDs
-	 * use this basically to hide and visible pages
-	*/
-	bulletin.pageIDs = [];
-
-	var DBPagesContent = '';
-
-	$.each(DBs, function (index, DB) {
-		DBPagesContent += generateDBPage(index,DB);
-	});
-
-	document.getElementById("dhisBulletin").innerHTML = DBPagesContent;
-
-	displayOnePage("dbPage-5");
-};
-
-var generateDBPage = function(pageNo,DB){
-
-	var DBPageContent = '';
-	DBPageContent += '<div id="dbPage-'+pageNo+'" class="page">';
-
-	$.each(DB.dashboardItems, function (index, DBItem) {
-		DBPageContent += generateDBItem(pageNo,index,DBItem);
-	});
-
-	DBPageContent += '<div class="inline">'+
-						'<textarea rows="4" cols="50">'+
-							'this is a text area template...'+
-						'</textarea>'+
-					'</div>';
-
-	DBPageContent += '</div>';
-
-	bulletin.pageIDs.push("dbPage-"+pageNo+"");
-
-	return DBPageContent;
-};
-
-var generateDBItem = function(pageNo,itemNo,DBItem){
-
-	var DBItemContent = '';
-	var itemType = getDBItemType(DBItem);
-
-	switch(itemType) {
-		case "reportTable":
-			DBItemContent = '<h4>reportTable</h4>';
-			break;
-		case "chart":
-			var itemID = DBItem.chart.id;
-			DBItemContent = '<div id="dbItem-'+pageNo+'-'+itemNo+'" class="dbItem inline">' +
-				'<img src="'+ getDBItemLink(itemType,itemID) +'" alt="db item">' +
-				'</div>';
-			break;
-		default:
-			DBItemContent = "invalid item";
-	}
-
-	return DBItemContent;
-};
-
-var getDBItemType = function(item){
-	var type = "invalid";
-
-	switch(item.type) {
-		case "reportTable":
-			type = "reportTable";
-			break;
-		case "chart":
-			type = "chart";
-			break;
-		default:
-		type = "invalid";
-	}
-
-	return type;
-};
-
-var getDBItemID = function(DBItem){
-	return DBItem.id;
-};
-
-var getDBItemLink = function(itemType,itemID){
-	var link = "";
-
-	switch(itemType) {
-		case "reportTable":
-			type = "reportTable";
-			break;
-		case "chart":
-			link = baseURL + "api/charts/" +itemID + "/data?width=410&height=275";
-			break;
-		default:
-			type = "invalid";
-	}
-
-	return link;
-};
-
-var displayOnePage = function(visibleID){
-
-	/*$.each(bulletin.pageIDs, function (index, pageID) {
-		if(pageID == visibleID)
-			$("#"+pageID+"").attr("style","display:initial");
-		else
-			$("#"+pageID+"").attr("style","display:none");
-	});*/
-
-	$(".page").addClass("hidden");
-	$("#"+visibleID+"").removeClass("hidden");
-
-};
-
-var renderPage
-
-$(document).ready(function(){
-
-	var def = $.Deferred();
-	var promise = def.promise();
-
-	promise = promise.then(loadDBsInfo);
-	promise = promise.then(function(){
-		console.log(bulletin.DBs);
-		generateDBPages(bulletin.DBs);
-	});
-
-	def.resolve();
-
-/*	$(window).on('hashchange', function(){
-		// On every hash change the render function is called with the new hash.
-		// This is how the navigation of our app happens.
-		loadPage(decodeURI(window.location.hash));
-		displayOnePage();
-	});*/
-
-
-});
+ var mv = document.getElementsByClassName('moveable');
+ console.log("hello..*.");
+ for (var i = 0; i < mv.length; i++) {
+ mv[i].addEventListener('dragstart', drag_start, false);
+ document.body.addEventListener('dragover', drag_over, false);
+ document.body.addEventListener('drop', drop, false);
+ }*/
